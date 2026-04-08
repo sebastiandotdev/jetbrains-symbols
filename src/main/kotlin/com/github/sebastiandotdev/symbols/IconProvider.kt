48eb66b06d72bd9ba02cdc7b5e867e5e02622562
Sebastián García <sebastiandotdev@gmail.com>
2026-04-07 23:06:29 -0500

    feat: enhance IconProvider

--- src/main/kotlin/com/github/sebastiandotdev/symbols/IconProvider.kt
+++ src/main/kotlin/com/github/sebastiandotdev/symbols/IconProvider.kt
@@ -1,7 +1,52 @@
 package com.github.sebastiandotdev.symbols
 
 import com.intellij.ide.IconProvider
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.PsiUtilCore
+import javax.swing.Icon
 
 class IconProvider : IconProvider() {
-
+  
+  override fun getIcon(element: PsiElement, flags: Int): Icon? {
+    val virtualFile = PsiUtilCore.getVirtualFile(element)
+    
+    val file = virtualFile?.let {
+      PsiManager.getInstance(element.project).findFile(it)
+    }
+    
+    return findIcon(virtualFile, file)
+  }
+  
+  private fun findIcon(virtualFile: VirtualFile?, file: PsiFile?): Icon? {
+    return when {
+      virtualFile?.isDirectory == true -> Icons.FOLDER_TO_ICON[virtualFile.name.lowercase()]
+        ?: Icons.folder
+      
+      else -> findFileIcon(virtualFile, file)
+    }
+  }
+  
+  private fun findFileIcon(virtualFile: VirtualFile?, file: PsiFile?): Icon? {
+    val fileName = virtualFile?.name?.lowercase()
+    
+    return Icons.FILE_TO_ICON[fileName]
+      ?: findExtensionIcon(fileName)
+  }
+  
+  private fun findExtensionIcon(fileName: String?): Icon? {
+    if (fileName == null) return null
+    
+    // We want to check for "test.tsx", "tsx" and "ts" extensions in that order.
+    // 1. "test.tsx"
+    // 2. "tsx"
+    val parts = fileName.split(".")
+    for (i in parts.indices) {
+      val ext = parts.subList(i, parts.size).joinToString(".")
+      Icons.EXT_TO_ICON[ext]?.let { return it }
+    }
+    return null
+  }
 }
